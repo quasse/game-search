ba503c6b498f8d8f87de9f4e0c287cbccfb63bb1
timothysepulvado <timothysepulvado@yahoo.com>
2021-09-30 19:42:46 -0700

    add nav and depend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Button from "@mui/material/Button"
+import AppBar from './AppBar'
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,15 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+       
+
+      </header> */}
+
+<AppBar>
+  
+</AppBar>
+
+      <Button variant="contained"> Button </Button>
     </div>
   );
 }
