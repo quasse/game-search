23405fd3b955d2fd84dd08d8dde6e54f2a4093fb
timothysepulvado <timothysepulvado@yahoo.com>
2021-10-02 11:33:46 -0700

    stash

--- client/src/Components/Games.js
+++ client/src/Components/Games.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Games = ({ game }) => (
-  <div className="games">
+  <div className="games" onClick={
+      console.log("hello") 
+  }> 
     <p>{game.name}</p>
     <img style={{width: '25vw'}} src={game.background_image} alt={game.name} />
   </div>
