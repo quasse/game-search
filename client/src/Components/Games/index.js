dbe0e84b8a814b13a54a81bfce795c8e1d34e043
Sam Morgen <samorgen14@gmail.com>
2021-10-03 06:05:31 -0700

    titles and images for profile page

--- client/src/Components/Games/index.js
+++ client/src/Components/Games/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-const Games = ({ game }) => (
-  <div className="games" onClick={
-      console.log("hello") 
-  }> 
-    <p>{game.name}</p>
-    <img
-      style={{ width: "25vw" }}
-      src={game.background_image}
-      alt={game.name}
-    />
-  </div>
-);
+const Games = ({ game }) => {
+  console.log("game ", game);
+  return (
+    <div className="games" onClick={console.log("hello")}>
+      <p>{game.name || game.title}</p>
+      <img
+        style={{ width: "25vw" }}
+        src={game.background_image || game.image}
+        alt={game.name}
+      />
+    </div>
+  );
+};
+
 export default Games;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
+import Games from "../Components/Games";
 
 const Profile = (props) => {
   const { loading, data } = useQuery(QUERY_ME);
 
-  console.log(loading);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   console.log("data: ", data);
 
   const user = data?.me || {};
 
+  console.log("user ", user.games);
+
   return (
     <div>
       <h2>Viewing{` ${user.username}'s profile`}</h2>
+      <div></div>
+      <div>
+        {user.games.length > 0 &&
+          user.games.map((game) => <Games game={game} />)}
+      </div>
     </div>
   );
 };
