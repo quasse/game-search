221f1dd6ab2158ccf87fca2959b5f636d4e05f3e
timothysepulvado <timothysepulvado@yahoo.com>
2021-10-02 20:31:58 -0700

    baisc style and new search bar

--- client/src/AppBar.js
+++ client/src/AppBar.js
@@ -1,68 +1,33 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
+
 import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
+
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import "./AppBar.css";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
+import SearchIcon from "@mui/icons-material/Search";
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+import InputBase from "@mui/material/InputBase";
+import { styled, alpha } from "@mui/material/styles";
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 export default function PrimarySearchAppBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  // create a state for the user input
-  const [searchInput, setSearchInput] = React.useState("");
+
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -179,9 +144,9 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: "none", sm: "block" } }}
           >
-            ATTS Games BOOOOOOOO!!!!!!!
+            ATTS Games
           </Typography>
-          <Search>
+          {/* <Search>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
@@ -191,7 +156,7 @@ export default function PrimarySearchAppBar() {
               placeholder="Search…"
               inputProps={{ "aria-label": "search" }}
             />
-          </Search>
+          </Search> */}
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {/* <IconButton size="large" aria-label="show 4 new mails" color="inherit"> */}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,17 @@
-import react, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Games from "../Components/Games";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import { styled, alpha } from "@mui/material/styles";
 //apollo queries (Troy)
-import { useQuery } from '@apollo/client';
-import { useMutation } from '@apollo/client';
-import { QUERY_USERS } from '../utils/queries';
-import { ADD_USER } from '../utils/mutations';
-import { LOGIN_USER } from '../utils/mutations';
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+import { QUERY_USERS } from "../utils/queries";
+import { ADD_USER } from "../utils/mutations";
+import { LOGIN_USER } from "../utils/mutations";
 
-  // apollo function that returns loading and data properties(Troy)
-  // asynchronous, loading property indicates if fetch is complete or not
+// apollo function that returns loading and data properties(Troy)
+// asynchronous, loading property indicates if fetch is complete or not
 // function QueryUsers(){
 //   const { loading, data } = useQuery(QUERY_USERS);
 //   if (!loading){
@@ -49,16 +52,56 @@ import { LOGIN_USER } from '../utils/mutations';
 // }
 // };
 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
 const Home = () => {
-
-
   // QueryUsers();
   // AddUser();
   // LoginUser();
 
-
   // game state for api fetch
   const [games, setGames] = useState([]);
+
+    // create a state for the user input
+  const [searchInput, setSearchInput] = useState("");
 
   const GAME_API = `https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}&page1`;
 
@@ -78,8 +121,19 @@ const Home = () => {
   }, []);
 
   return (
-    <main>
-      <h1>Home page</h1>
+    <main classNmae="home-app" style={{backgroundColor: "#8e8e8e"}}>
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+        />
+      </Search>
+
       <div>
         {games.length > 0 && games.map((game) => <Games game={game} />)}
       </div>
