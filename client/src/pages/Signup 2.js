71b9509d66a2a95b0b9cd4a321f2900e293ce2e3
timothysepulvado <timothysepulvado@yahoo.com>
2021-10-07 18:14:29 -0700

    stash

--- client/src/pages/Signup 2.js
+++ client/src/pages/Signup 2.js
@@ -1,67 +1,67 @@
-import React, { useState } from "react";
-import { useMutation } from "@apollo/client";
-import { ADD_USER } from "../utils/mutations";
-import Auth from "../utils/auth";
+// import React, { useState } from "react";
+// import { useMutation } from "@apollo/client";
+// import { ADD_USER } from "../utils/mutations";
+// import Auth from "../utils/auth";
 
-const Signup = (props) => {
-  const [formState, setFormState] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
+// const Signup = (props) => {
+//   const [formState, setFormState] = useState({
+//     username: "",
+//     email: "",
+//     password: "",
+//   });
 
-  const [addUser, { error }] = useMutation(ADD_USER);
+//   const [addUser, { error }] = useMutation(ADD_USER);
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+//     setFormState({
+//       ...formState,
+//       [name]: value,
+//     });
+//   };
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+//   const handleFormSubmit = async (event) => {
+//     event.preventDefault();
 
-    try {
-      const { data } = await addUser({ variables: { ...formState } });
+//     try {
+//       const { data } = await addUser({ variables: { ...formState } });
 
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
+//       Auth.login(data.addUser.token);
+//     } catch (e) {
+//       console.error(e);
+//     }
 
-    setFormState({ email: "", username: "", password: "" });
-  };
+//     setFormState({ email: "", username: "", password: "" });
+//   };
 
-  return (
-    <form onSubmit={handleFormSubmit}>
-      <input
-        className="form-input"
-        placeholder="Email"
-        name="email"
-        value={formState.email}
-        onChange={handleChange}
-      />
-      <input
-        className="form-input"
-        placeholder="User Name"
-        name="username"
-        value={formState.username}
-        onChange={handleChange}
-      />
-      <input
-        className="form-input"
-        placeholder="Password"
-        name="password"
-        type="password"
-        value={formState.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+//   return (
+//     <form onSubmit={handleFormSubmit}>
+//       <input
+//         className="form-input"
+//         placeholder="Email"
+//         name="email"
+//         value={formState.email}
+//         onChange={handleChange}
+//       />
+//       <input
+//         className="form-input"
+//         placeholder="User Name"
+//         name="username"
+//         value={formState.username}
+//         onChange={handleChange}
+//       />
+//       <input
+//         className="form-input"
+//         placeholder="Password"
+//         name="password"
+//         type="password"
+//         value={formState.password}
+//         onChange={handleChange}
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// };
 
-export default Signup;
+// export default Signup;
